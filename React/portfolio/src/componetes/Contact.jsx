baa21a9a53581abf77a360026a4f7976f58b8274
agustin <tapieagus@gmail.com>
2023-05-09 02:03:19 -0300

    tp 1 y 2 realizados en uno solo

--- React/portfolio/src/componetes/Contact.jsx
+++ React/portfolio/src/componetes/Contact.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom';
 
 const Contact = () => {
   return (
@@ -6,18 +6,24 @@ const Contact = () => {
                   <div className="form2">
                         <h1>Contacto:</h1>
                         <form  className="form1">
-                                
                                 <label className="label_form">Nombre:</label><input type="text" placeholder="Ingrese su nombre"/><br />
                                 <label className="label_form">Apellido:</label><input type="text" placeholder="Ingrese su Apellido"/><br />
                                 <label className="label_form">Email:</label><input type="email" placeholder="Ingrese su Email"/><br />
                                 <label className="label_form">Numero de contacto:</label><input type="text" placeholder="Ingrese su numero"/><br />
                                 <button className="buton_form">Enviar</button>
-                        </form>
-                          
-                          
+                        </form>   
+                  </div>  
+
+                  <div className='nav'>
+                      <nav className="navbar navbar-light bg-light">
+                      <Link to="/" className='boton_contacto'>Volver a inicio</Link>
+                      </nav>
                   </div>
-                    
+
+
                   </body>
+
+  
   )
 }
 
